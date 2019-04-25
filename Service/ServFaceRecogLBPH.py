8972cc8472bc46ae9724899e65ae683b31d90f68
dexter0ion@gmail.com <dexter0ion@gmail.com>
2019-04-26 02:21:57 +0800

    postPortrait method

--- Service/ServFaceRecogLBPH.py
+++ Service/ServFaceRecogLBPH.py
@@ -167,40 +167,43 @@ class ServFaceRecogLBPH():
 
         #post portrait
         if self.switchFlag['PostPortrait'] == True:
-            #init portrait dict
-            
-            portraits = {"plist":[]}
-            #load portrait
-            print("[Load Portrait]")
-            for portrait in os.listdir(r"./portrait"):              
-                pname = portrait[:-4]
-                print('./portrait/'+portrait)
-                with open('./portrait/'+portrait, 'rb') as f:
-                     img=base64.b64encode(f.read())
-                pdata = img.decode('utf-8') #将image2str转为str
-                '''
-                pdata = cv2.imread('./portrait/'+portrait)
-                '''
-                print("pdata type="+str(type(pdata)))
-                
-                #ndarray to list
-                #pdata = pdata.tolist()
-                print("pdata type="+str(type(pdata)))
-                #add to dict
-                tmpPDict={"name":pname,"data":pdata}
-                portraits["plist"].append(tmpPDict)
-                #print("portraits dict:"+str(portraits))
-            print("[Load Portrait]-PASS") 
-            print('[Post Portraits JSON]') 
-            pJson = json.dumps(portraits)
-            #print(pJson)
-            post_portrait = ServHttp('POST','http://127.0.0.1:5000/api/portrait',portraits)
-            try:
-                post_portrait.process()
-            except:
-                print("[Portraits Json]-传输失败")
-            else:
-                print("[Portraits Json]-传输成功")   
+           self.postPortrait()
+
+    def postPortrait(self):
+        #init portrait dict
+
+        portraits = {"plist":[]}
+        #load portrait
+        print("[Load Portrait]")
+        for portrait in os.listdir(r"./portrait"):              
+            pname = portrait[:-4]
+            print('./portrait/'+portrait)
+            with open('./portrait/'+portrait, 'rb') as f:
+                 img=base64.b64encode(f.read())
+            pdata = img.decode('utf-8') 
+            '''
+            pdata = cv2.imread('./portrait/'+portrait)
+            '''
+            print("pdata type="+str(type(pdata)))
+
+            #ndarray to list
+            #pdata = pdata.tolist()
+            print("pdata type="+str(type(pdata)))
+            #add to dict
+            tmpPDict={"name":pname,"data":pdata}
+            portraits["plist"].append(tmpPDict)
+            #print("portraits dict:"+str(portraits))
+        print("[Load Portrait]-PASS") 
+        print('[Post Portraits JSON]') 
+        pJson = json.dumps(portraits)
+        #print(pJson)
+        post_portrait = ServHttp('POST','http://127.0.0.1:5000/api/portrait',portraits)
+        try:
+            post_portrait.process()
+        except:
+            print("[Portraits Json]-传输失败")
+        else:
+            print("[Portraits Json]-传输成功")   
 
      
     def cutPortrait(self, x, y, w, h, this_name):
--- todo.md
+++ todo.md
@@ -3,4 +3,44 @@ April 2019
 
 ### Portrait传输
 
+```
+    def postPortrait(self):
+        #init portrait dict
+
+        portraits = {"plist":[]}
+        #load portrait
+        print("[Load Portrait]")
+        for portrait in os.listdir(r"./portrait"):              
+            pname = portrait[:-4]
+            print('./portrait/'+portrait)
+            with open('./portrait/'+portrait, 'rb') as f:
+                 img=base64.b64encode(f.read())
+            pdata = img.decode('utf-8') 
+            '''
+            pdata = cv2.imread('./portrait/'+portrait)
+            '''
+            print("pdata type="+str(type(pdata)))
+
+            #ndarray to list
+            #pdata = pdata.tolist()
+            print("pdata type="+str(type(pdata)))
+            #add to dict
+            tmpPDict={"name":pname,"data":pdata}
+            portraits["plist"].append(tmpPDict)
+            #print("portraits dict:"+str(portraits))
+        print("[Load Portrait]-PASS") 
+        print('[Post Portraits JSON]') 
+        pJson = json.dumps(portraits)
+        #print(pJson)
+        post_portrait = ServHttp('POST','http://127.0.0.1:5000/api/portrait',portraits)
+        try:
+            post_portrait.process()
+        except:
+            print("[Portraits Json]-传输失败")
+        else:
+            print("[Portraits Json]-传输成功")   
+
+
+```
+
 ### 邮件通知
